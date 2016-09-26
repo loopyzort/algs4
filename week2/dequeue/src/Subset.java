6a79ce65e02294ef9eb4d04a9f648faab97cd0da
Todd Santaniello <loopyzort@gmail.com>
2016-09-26 16:48:02 -0700

    Adding Subset implementation

---
+++ week2/dequeue/src/Subset.java
@@ -0,0 +1,15 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Subset {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> random = new RandomizedQueue<>();
+        while(!StdIn.isEmpty()) {
+            random.enqueue(StdIn.readString());
+        }
+        for (int i = 0; i < k; i++) {
+            StdOut.println(random.dequeue());
+        }
+    }
+}
