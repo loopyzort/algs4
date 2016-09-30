46d3d8db12a6a377c7188eff900e24f36aa9c949
Todd Santaniello <loopyzort@gmail.com>
2016-09-29 22:21:06 -0700

    Implementing BruteCollinearPoints, which is hideous

--- week3/collinear-points/src/BruteCollinearPoints.java
+++ week3/collinear-points/src/BruteCollinearPoints.java
@@ -6,18 +6,62 @@ import edu.princeton.cs.algs4.StdOut;
  *
  */
 public class BruteCollinearPoints {
+    private LineSegment[] segments;
+
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new NullPointerException("constructor argument is null");
+        }
+        // initialize it to be too big, but shrink when done
+        LineSegment[] tmp = new LineSegment[points.length];
+        int i = 0;
+        for (int p = 0; p < points.length - 3; p++) {
+            Point one = points[p];
+            if (one == null) {
+                throw new NullPointerException("Null point at index: " + p);
+            }
+            for (int q = p + 1; q < points.length - 2; q++) {
+                Point two = points[q];
+                if (two == null) {
+                    throw new NullPointerException("Null point at index: " + q);
+                }
+                for (int r = q + 1; r < points.length - 1; r++) {
+                    Point three = points[r];
+                    if (three == null) {
+                        throw new NullPointerException("Null point at index: " + r);
+                    }
+                    for (int s = r + 1; s < points.length; s++) {
+                        Point four = points[s];
+                        if (four == null) {
+                            throw new NullPointerException("Null point at index: " + s);
+                        }
+                        if (one.slopeTo(two) == one.slopeTo(three) &&
+                                one.slopeTo(two) == one.slopeTo(four)) {
+                            Point smallest = one.compareTo(two) < 0 ? one : two;
+                            smallest = smallest.compareTo(three) < 0 ? smallest : three;
+                            smallest = smallest.compareTo(four) < 0 ? smallest : four;
+                            Point biggest = one.compareTo(two) > 0 ? one : two;
+                            biggest = biggest.compareTo(three) > 0 ? biggest : three;
+                            biggest = biggest.compareTo(four) > 0 ? biggest : four;
+                            tmp[i] = new LineSegment(smallest, biggest);
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+        segments = java.util.Arrays.copyOf(tmp, i);
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return 0;
+        return segments.length;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        return null;
+        return segments;
     }
 
     public static void main(String[] args) {
