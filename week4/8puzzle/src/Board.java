f37b75b6e1e1b72535ca3b89a4263ba4f404532f
Todd Santaniello <loopyzort@gmail.com>
2016-10-03 12:53:45 -0700

    starting the 8puzzle impl

--- week4/8puzzle/src/Board.java
+++ week4/8puzzle/src/Board.java
@@ -1,11 +1,16 @@
+import java.util.Arrays;
+
 public class Board {
+    private final int[][] blocks;
+
     // construct a board from an n-by-narray of blocks (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
+        this.blocks = blocks;
     }
 
     // board dimension n
     public int dimension() {
-        return 0;
+        return blocks.length;
     }
 
     // number of blocks out of place
@@ -30,7 +35,19 @@ public class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
-        return false;
+        if (!(y instanceof Board)) {
+            return false;
+        }
+        Board comp = (Board) y;
+        if (comp.dimension() != comp.dimension()) {
+            return false;
+        }
+        for (int i = 0; i < dimension(); i++) {
+            if (!Arrays.equals(blocks[i], comp.blocks[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     // all neighboring boards
@@ -40,11 +57,30 @@ public class Board {
 
     // string representation of this board (in the output format specified below)
     public String toString() {
-        return null;
+        StringBuilder builder = new StringBuilder().append(dimension()).append("\n");
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
+                builder.append(" ").append(blocks[i][j]);
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
     }
 
 
     // unit tests (not graded)
     public static void main(String[] args) {
+        // test equality
+        int[][] data = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+        Board subject = new Board(data);
+        assert !(subject.equals("bunk"));
+        assert !(subject.equals(null));
+        assert subject.equals(new Board(data));
+        Board smaller = new Board(new int[][]{{1, 2}, {3, 4}});
+        assert !(subject.equals(smaller));
+        int[][] differentData = new int[][]{{1, 3, 2}, {4, 5, 6}, {7, 8, 0}};
+        Board different = new Board(differentData);
+        assert !(subject.equals(different));
+
     }
 }
